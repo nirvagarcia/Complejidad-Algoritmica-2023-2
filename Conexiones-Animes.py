b96e7d603c7830ca9afe78068b6aec0552315891
The Editor <68383325+Marpellic@users.noreply.github.com>
2023-09-28 02:57:08 -0500

    Add files via upload

--- Conexiones-Animes.py
+++ Conexiones-Animes.py
@@ -1,46 +1,122 @@
+﻿import networkx as nx
 import pandas as pd
-from sklearn.metrics.pairwise import euclidean_distances
+import matplotlib.pyplot as plt
+import math
 
-# Cargar los datos desde el archivo CSV
-data = pd.read_csv('anime.csv', delimiter=';')
+Grafo = nx.Graph()
 
-# Crear un DataFrame para las conexiones
-connections = {'source': [], 'target': []}
+data = pd.read_csv('anime_data.csv', delimiter=";")
 
-# Crear un diccionario para almacenar los géneros de cada anime
-genre_dict = {}
+for index, row in data.iterrows():
+    anime_title = row['titleEnglish']
+    anime_attributes = {
+        'titleRomaji': row['titleRomaji'],
+        'startDate': row['startDate'],
+        'endDate': row['endDate'],
+        'format': row['format'],
+        'genres': eval(row['genres']),
+        'popularity': row['popularity'],
+        'score': row['score'],
+        'image': "/home/marcelo/documentation/" + row['image']
+    }
+    Grafo.add_node(anime_title, **anime_attributes)
 
-# Construir el diccionario de géneros
-for i, row in data.iterrows():
-    genres = set(row['genres'].split(';'))
-    genre_dict[row['id']] = genres
 
-# Dividir los animes en grupos basados en géneros y puntuación
-genre_groups = {}
-for i, row in data.iterrows():
-    genres = tuple(sorted(row['genres'].split(';')))
-    score = row['score']
-    key = (genres, score)
-    if key not in genre_groups:
-        genre_groups[key] = []
-    genre_groups[key].append(row['id'])
+def calcular_valor_relacion(anime1, anime2):
+    generos_anime1 = set(Grafo.nodes[anime1]['genres'])
+    generos_anime2 = set(Grafo.nodes[anime2]['genres'])
+    generos_comunes = generos_anime1.intersection(generos_anime2)
+    generos = generos_anime1.union(generos_anime2)
+    popularidad_anime1 = Grafo.nodes[anime1]['popularity']
+    popularidad_anime2 = Grafo.nodes[anime2]['popularity']
+    diferencia_popularidad = abs(popularidad_anime1 - popularidad_anime2)
+    sumapopularidad = popularidad_anime1 + popularidad_anime2
+    valor_relacion = (len(generos)*2 + (diferencia_popularidad/sumapopularidad))*4 / (1.1 + len(generos_comunes)*3)
+    if len(generos_comunes): return valor_relacion;
+    else: return 0;
 
-# Iterar a través de los grupos y encontrar conexiones dentro de cada grupo
-for group_animes in genre_groups.values():
-    for i in range(len(group_animes)):
-        for j in range(i + 1, len(group_animes)):
-            # Comprobar si cumplen con las restricciones de puntuación
-            score_i = data.loc[data['id'] == group_animes[i], 'score'].values[0]
-            score_j = data.loc[data['id'] == group_animes[j], 'score'].values[0]
-            score_difference = abs(score_i - score_j)
-            
-            if score_difference <= 10: 
-                # Agregar la conexión al DataFrame
-                connections['source'].append(group_animes[i])
-                connections['target'].append(group_animes[j])
+cantRelations = 0
+for anime1 in Grafo.nodes:
+    for anime2 in Grafo.nodes:
+        if anime1 != anime2:
+            valor_relacion = calcular_valor_relacion(anime1, anime2)
+            if valor_relacion > 0:
+                cantRelations += 1
+                Grafo.add_edge(anime1, anime2, weight=valor_relacion)
+print(cantRelations)
 
-# Crear un DataFrame a partir del diccionario de conexiones
-connections_df = pd.DataFrame(connections)
+def dijkstra(graph, start, end):
+    peso_final_del_camino = 0
+    distancias = {}
+    for node in graph.nodes:
+        distancias[node] = math.inf
+    if start in distancias and end in distancias:
+        distancias[start] = 0
+        camino_mas_corto = {};
+        for node in graph.nodes:
+            camino_mas_corto[node] = None
 
-# Guardar las conexiones en un archivo CSV
-connections_df.to_csv('anime_connections.csv', index=False)
+        nodos_no_visitados = list(graph.nodes)
+
+        while nodos_no_visitados:
+            nodo_actual = None
+            distancia_minima = math.inf
+            for node in nodos_no_visitados:
+                if distancias[node] < distancia_minima:
+                    nodo_actual = node
+                    distancia_minima = distancias[node]
+
+            # Si llegamos al nodo de destino, construye y devuelve el camino
+            if nodo_actual == end:
+                camino = []
+                while nodo_actual is not None:
+                    camino.insert(0, nodo_actual)
+                    nodo_actual = camino_mas_corto[nodo_actual]
+                return camino, peso_final_del_camino
+
+            nodos_no_visitados.remove(nodo_actual)
+
+            # Actualiza las distancias y los caminos para los nodos vecinos
+            for vecino in graph.neighbors(nodo_actual):
+                distancia_alternativa = distancias[nodo_actual] + graph[nodo_actual][vecino]['weight']
+
+                if distancia_alternativa < distancias[vecino]:
+                    distancias[vecino] = distancia_alternativa
+                    camino_mas_corto[vecino] = nodo_actual
+                    if vecino == end:
+                        peso_final_del_camino = distancia_alternativa
+
+    return None, peso_final_del_camino
+
+
+def calcularDijkstra(a,b):
+    camino, peso = dijkstra(Grafo, a, b)
+    if camino:
+        print(f"Ruta más corta entre {a} y {b}:")
+        for node in camino:
+            print(node)
+        porcentajeRecomendacion = round(-5.64 * peso + 113.43,0)
+        print(f"El porcentaje de recomendacion del anime {b} con respecto al anime {a} es del {porcentajeRecomendacion}%")
+    else:
+        print(f"No se encontró una ruta entre {a} y {b}. Posiblemente se deba a que {a} o {b} no esten en la base de datos.")
+
+print("Para salir e imprimir el grafo en un archivo llamado grafo.png, escriba salir en cualquier input")
+while(True):
+
+    a = input("Ingrese el nombre del anime 1:")
+    b = input("Ingrese el nombre del anime 2:")
+    if a.lower() == "salir" or b.lower() == "salir":
+        break
+    else:
+        calcularDijkstra(a, b)
+
+
+
+layout_params = {
+    'prog': 'neato',
+    'args': '-Gscale=40.0',
+}
+
+A = nx.nx_agraph.to_agraph(Grafo)
+A.layout(**layout_params)
+A.draw('grafo.png')
