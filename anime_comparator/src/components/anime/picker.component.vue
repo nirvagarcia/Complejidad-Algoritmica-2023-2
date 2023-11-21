2156fd43be0eec401cd2ab57c089654935a9f3c5
The Editor <68383325+Marpellic@users.noreply.github.com>
2023-11-21 11:36:27 -0500

    Feat: Implemented search bar in picker component

--- anime_comparator/src/components/anime/picker.component.vue
+++ anime_comparator/src/components/anime/picker.component.vue
@@ -4,16 +4,18 @@ export default {
   data(){
     return{
       animeData: [],
+      filteredAnimeData: [],
       loadedAnimeData: [],
-      toLoad: 10,
+      toLoad: 20,
+      textFilter: "",
     }
   },
   methods:{
     loadMore(){
       let limit = this.toLoad + this.loadedAnimeData.length
-      if(limit > this.animeData.length) limit = this.animeData.length;
+      if(limit > this.filteredAnimeData.length) limit = this.filteredAnimeData.length;
       for(let i = this.loadedAnimeData.length; i < limit; i++){
-        this.loadedAnimeData.push(this.animeData[i]);
+        this.loadedAnimeData.push(this.filteredAnimeData[i]);
       }
     },
     select(id: number){
@@ -27,19 +29,31 @@ export default {
           for(let i = 0; i < imageList.length; i++){
             this.animeData.push({  name: nameList[i], image: imageList[i], id: i  })
           }
+          this.filteredAnimeData = this.animeData
           for(let i = 0; i < this.toLoad && i < this.animeData.length; i++){
             this.loadedAnimeData.push(this.animeData[i]);
           }
         }
       })
     })
-
+  },
+  watch: {
+    textFilter(newValue, oldValue) {
+      this.filteredAnimeData = this.animeData.filter(anime => anime.name.toLowerCase().includes(newValue.toLowerCase())).slice(0, this.toLoad);
+      this.loadedAnimeData = [];
+      this.loadMore()
+    }
   }
 }
 </script>
 
 <template>
   <div v-if="loadedAnimeData.length > 0">
+    <div class="flex-col align-center margin-10">
+      <div>Busca tus animes por nombre:</div>
+      <div><pv-input v-model="textFilter"/></div>
+
+    </div>
     <div class="flex-auto picker-container">
       <div v-for="data in loadedAnimeData" :key="data.id" @click="select(data.id)" class="picker-item">
         <pv-image :src="data.image" class="picker-img"/>
